4449b275ba198385fcb1b03049890f340c58db16
Eylon B.Simon <73744890+eylon696@users.noreply.github.com>
2021-02-20 20:23:14 +0200

    Add files via upload

---
+++ Lab 3/BankTest/BankAccountTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Lab_4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTest
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        private PrivateObject privateObject;
+        BankAccount bankAccount = new BankAccount("gg", 5);
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            privateObject = new PrivateObject(typeof(BankAccount));
+        }
+        [TestMethod]
+        public void CheckFreezeAccount()
+        {
+            bool m_frozen = (bool)privateObject.GetField("m_frozen");
+            Assert.IsFalse(m_frozen);
+        }
+
+        [TestMethod]
+        public void TestPrivateFreeze()
+        {
+            privateObject.Invoke("FreezeAccount");
+            bool checkFrozen = (bool)privateObject.GetField("m_frozen");
+            Assert.IsTrue(checkFrozen);
+        }
+    }
+}
+
